0f2d8e54d1108e90df7cee63c3f8a8797eb2eae7
charmainemimie <charmainemangorima@gmail.com>
2026-05-23 00:35:31 +0200

    add product with image

--- src/main/java/com/mimie/ecommerceBackend/controller/ProductController.java
+++ src/main/java/com/mimie/ecommerceBackend/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 @CrossOrigin
@@ -51,8 +52,17 @@ public class ProductController {
 
     }
 
-//    @PostMapping("/products")
-//    public List<Product>getAllProducts() {
-//        return service.getAllProducts();
-//    }
-}
+    //add product
+   @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+        try {
+            System.out.println(product);
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+   }
